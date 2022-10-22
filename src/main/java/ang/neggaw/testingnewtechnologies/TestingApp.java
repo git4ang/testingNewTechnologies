8e5eb6f3aeca72718d5605cfa346128071edc53a
git4ang <code4ang@gmail.com>
2022-10-22 10:05:41 +0200

    project initial

---
+++ src/main/java/ang/neggaw/testingnewtechnologies/TestingApp.java
@@ -0,0 +1,13 @@
+package ang.neggaw.testingnewtechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingApp.class, args);
+    }
+
+}
---
+++ src/test/java/ang/neggaw/testingnewtechnologies/TestingAppTests.java
@@ -0,0 +1,13 @@
+package ang.neggaw.testingnewtechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
